cc4730c8d877c116da7c67dbfe727551a2435126
kelzuuuu <johnmichaellevsantos82@gmail.com>
2025-06-17 07:17:59 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/gwacalculator/GwaCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gwacalculator;
+
+/**
+ *
+ * @author Michaelle
+ */
+public class GwaCalculator {
+
+    public static void main(String[] args) {
+        
+        GwaCalcs gwa = new GwaCalcs();
+        gwa.show();
+        
+    }
+}
